f3c9c94b53467880897347809205d13bf55daf13
Luiz Guilherme Costa da Silva <luiz.developer.acc@gmail.com>
2022-05-26 22:26:27 -0300

    added variables, solved loop problems and added fib.dumb into examples

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,15 @@ use std::env;
 use std::fs;
 use std::process;
 
-const KEYWORDS: &'static [&'static str] = &["print", "dup", "swap", "over", "drop", "loop", "end"];
+const KEYWORDS: &'static [&'static str] =
+    &["print", "dup", "swap", "over", "drop", "->", "loop", "end"];
 const OPERATIONS: &'static [&'static str] = &["+", "-", "*", "/", "%"];
+const TYPES: &'static [&'static str] = &["int"];
+
+struct Declaration {
+    identifier: String,
+    value: i64,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -36,8 +43,8 @@ fn main() {
     tokenizer(&contents, &mut tokens);
 
     let mut stack: Vec<i64> = Vec::new();
-
-    interpreter(&mut stack, &tokens);
+    let mut declarations: Vec<Declaration> = Vec::new();
+    interpreter(&mut stack, &tokens, &mut declarations);
 }
 
 fn tokenizer(contents: &String, tokens_vector: &mut Vec<String>) {
@@ -68,73 +75,101 @@ fn is_separator(ch: char) -> bool {
     }
 }
 
-fn interpreter(stack: &mut Vec<i64>, tokens: &Vec<String>) {
+fn interpreter(stack: &mut Vec<i64>, tokens: &Vec<String>, declarations: &mut Vec<Declaration>) {
     let mut token_slice_init: usize = 0;
     let mut token_slice_finish: usize;
 
-    const LOOP: u8 = 0;
-    const NOTHING: u8 = 1;
+    enum States {
+        LOOP,
+        ASSIGNMENT,
+        NOTHING,
+    }
 
-    let mut search_state = NOTHING;
+    let mut search_state = States::NOTHING as u8;
 
     for (i, token) in tokens.iter().enumerate() {
-        if token.chars().all(char::is_numeric) && search_state == NOTHING {
+        if token.chars().all(char::is_numeric) && search_state == States::NOTHING as u8 {
             stack.push(token.parse().unwrap());
         } else {
-            for (_j, keyword) in KEYWORDS.iter().enumerate() {
-                if token == keyword {
-                    if search_state == NOTHING {
-                        if keyword.to_string() == "print" {
-                            print_stack(stack)
-                        };
-                        if keyword.to_string() == "dup" {
-                            duplicate(stack)
-                        };
-                        if keyword.to_string() == "swap" {
-                            swap(stack)
-                        };
-                        if keyword.to_string() == "over" {
-                            over(stack)
-                        };
-                        if keyword.to_string() == "drop" {
-                            drop_top(stack)
-                        };
-                        if keyword.to_string() == "loop" {
-                            token_slice_init = i + 1;
-                            search_state = LOOP;
-                        }
+            let find_in_keywords = KEYWORDS.iter().find(|&x| x == token);
+            let find_in_keywords = match find_in_keywords {
+                Option::Some(result) => result,
+                Option::None => "Not Found",
+            };
+
+            if find_in_keywords != "Not Found" {
+                if search_state == States::NOTHING as u8 {
+                    if find_in_keywords == "print" {
+                        print_stack(stack);
+                    } else if find_in_keywords == "dup" {
+                        duplicate(stack);
+                    } else if find_in_keywords == "swap" {
+                        swap(stack);
+                    } else if find_in_keywords == "over" {
+                        over(stack);
+                    } else if find_in_keywords == "drop" {
+                        drop_top(stack);
+                    } else if find_in_keywords == "loop" {
+                        token_slice_init = i + 1;
+                        search_state = States::LOOP as u8;
+                    } else if find_in_keywords == "->" {
+                        token_slice_init = i + 1;
+                        search_state = States::ASSIGNMENT as u8;
                     }
-
-                    if keyword.to_string() == "end" {
+                } else {
+                    if find_in_keywords == "end" {
                         token_slice_finish = i;
-                        if search_state == LOOP {
-                            loop_instructions(stack, &tokens[token_slice_init..token_slice_finish]);
-                        };
-                        search_state = NOTHING;
+                        if search_state == States::LOOP as u8 {
+                            let slice = &tokens[token_slice_finish..];
+                            let mut prev_indx_end_was_encountered = 0;
+                            for (j, new_token) in slice.iter().enumerate() {
+                                if new_token == "end" {
+                                    token_slice_finish += j - prev_indx_end_was_encountered;
+                                    prev_indx_end_was_encountered = j;
+                                }
+                            }
+                            loop_instructions(
+                                stack,
+                                &tokens[token_slice_init..token_slice_finish],
+                                declarations,
+                            );
+                        } else if search_state == States::ASSIGNMENT as u8 {
+                            assignment(
+                                stack,
+                                declarations,
+                                &tokens[token_slice_init..token_slice_finish],
+                            );
+                        }
+                        search_state = States::NOTHING as u8;
                     };
-                    break;
                 }
+                continue;
             }
-            for operation in OPERATIONS.iter() {
-                if search_state == NOTHING {
-                    if token == operation {
-                        if operation.to_string() == "+" {
-                            sum(stack)
-                        };
+            if search_state == States::NOTHING as u8 {
+                let find_in_operations = OPERATIONS.iter().find(|&x| x == token);
+                let find_in_operations = match find_in_operations {
+                    Option::Some(result) => result,
+                    Option::None => "Not Found",
+                };
 
-                        if operation.to_string() == "-" {
-                            sub(stack)
-                        };
-                        if operation.to_string() == "*" {
-                            multy(stack)
-                        };
-                        if operation.to_string() == "/" {
-                            div(stack)
-                        };
-                        if operation.to_string() == "%" {
-                            rdiv(stack)
-                        };
-                        break;
+                if find_in_operations != "Not Found" {
+                    if find_in_operations == "+" {
+                        sum(stack);
+                    } else if find_in_operations == "-" {
+                        sub(stack);
+                    } else if find_in_operations == "*" {
+                        multy(stack);
+                    } else if find_in_operations == "/" {
+                        div(stack);
+                    } else if find_in_operations == "%" {
+                        rdiv(stack);
+                    };
+                    continue;
+                }
+
+                for declaration in declarations.iter() {
+                    if *token == declaration.identifier {
+                        stack.push(declaration.value);
                     }
                 }
             }
@@ -213,7 +248,7 @@ fn sum(stack: &mut Vec<i64>) {
         );
         process::exit(-1);
     }
-    
+
     let (result, overflow): (i64, bool) = stack[len - 2].overflowing_add(stack[len - 1]);
 
     if overflow {
@@ -244,8 +279,6 @@ fn sub(stack: &mut Vec<i64>) {
         process::exit(-1);
     }
 
-
-
     stack.pop();
     stack.pop();
     stack.push(result);
@@ -268,7 +301,6 @@ fn multy(stack: &mut Vec<i64>) {
         println!("Multiplication exceeded the limits of i64!!!");
         process::exit(-1);
     }
-
 
     stack.pop();
     stack.pop();
@@ -293,7 +325,6 @@ fn div(stack: &mut Vec<i64>) {
         process::exit(-1);
     }
 
-
     stack.pop();
     stack.pop();
     stack.push(result);
@@ -316,7 +347,63 @@ fn rdiv(stack: &mut Vec<i64>) {
     stack.push(result);
 }
 
-fn loop_instructions(stack: &mut Vec<i64>, instructions: &[String]) {
+fn assignment(stack: &mut Vec<i64>, declarations: &mut Vec<Declaration>, token_slice: &[String]) {
+    if token_slice.len() < 2 {
+        println!("Too few arguments to variable assignment!");
+        process::exit(-1);
+    } else if token_slice.len() > 2 {
+        println!("Too much arguments to variable assignment!");
+        process::exit(-1);
+    }
+    let valid_type: bool;
+    let result = TYPES.iter().find(|x| x == &&token_slice[1].as_str());
+    match result {
+        Some(_p) => valid_type = true,
+        None => valid_type = false,
+    };
+
+    if !valid_type {
+        println!("Invalid type!");
+        process::exit(-1);
+    }
+
+    if stack.len() < 1 {
+        println!("No elements in the stack for assignment");
+        process::exit(-1);
+    }
+    let check_if_identifier_is_valid = token_slice[0].chars().all(char::is_alphanumeric);
+
+    if !check_if_identifier_is_valid {
+        println!("Identifier {} is not a valid one", token_slice[0]);
+        process::exit(-1);
+    }
+
+    let mut identifier_already_declared = false;
+
+    for (indice, declaration) in declarations.iter().enumerate() {
+        if declaration.identifier == token_slice[0] {
+            identifier_already_declared = true;
+            declarations[indice].value = stack[stack.len() - 1];
+            break;
+        }
+    }
+
+    if !identifier_already_declared {
+        let new_declaration = Declaration {
+            identifier: token_slice[0].to_string(),
+            value: stack[stack.len() - 1],
+        };
+        declarations.push(new_declaration);
+    }
+
+    stack.pop();
+}
+
+fn loop_instructions(
+    stack: &mut Vec<i64>,
+    instructions: &[String],
+    declarations: &mut Vec<Declaration>,
+) {
     let len = stack.len();
 
     if len < 1 {
@@ -329,7 +416,7 @@ fn loop_instructions(stack: &mut Vec<i64>, instructions: &[String]) {
     stack.pop();
 
     while counter < times_to_loop {
-        interpreter(stack, &instructions.to_vec());
+        interpreter(stack, &instructions.to_vec(), declarations);
         counter += 1;
     }
 }
